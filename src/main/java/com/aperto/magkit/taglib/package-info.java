0a648afe56c9fc3348dfdb1a0f79d7aa44c13d63
Frank Sommer <frank.sommer@aperto.de>
2008-04-10 08:49:11 +0000

    change mvn structure

--- src/main/java/com/aperto/magkit/taglib/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * <h2>Aperto Magnolia Tag Library</h2>
- */
-@org.apache.myfaces.tobago.apt.annotation.Taglib(
-    jspVersion = "1.2", tlibVersion = "2.1",
-    shortName = "mk", uri = "http://www.aperto.de/",
-    fileName = "magkit-taglib.tld", displayName = "Aperto Magnolia Tags")
-package com.aperto.magkit.taglib;
