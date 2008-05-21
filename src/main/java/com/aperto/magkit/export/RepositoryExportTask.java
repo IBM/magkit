a9f90850a1eda4386f39f9683f714c99c3b8cd09
Frank Sommer <frank.sommer@aperto.de>
2008-05-21 07:47:59 +0000

    remove debug suite dependencies

--- src/main/java/com/aperto/magkit/export/RepositoryExportTask.java
+++ src/main/java/com/aperto/magkit/export/RepositoryExportTask.java
@@ -171,7 +171,7 @@ public class RepositoryExportTask extends MatchingTask {
         URL url;
         String[] elements;
         try {
-            url = new URL("http", _targetHost, _targetPort, getWebapp() + "/debug/get_node_children.jsp?currentNode=" + currentSite + getLogin());
+            url = new URL("http", _targetHost, _targetPort, getWebapp() + "/docroot/magkit/get_node_children.jsp?currentNode=" + currentSite + getLogin());
             info("access: " + url.toExternalForm());
             HttpClient httpClient = new HttpClient();
             GetMethod getMethod = new GetMethod(url.toString());
