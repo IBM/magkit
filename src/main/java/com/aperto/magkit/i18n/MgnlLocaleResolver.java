9640d977ea72bf0f892a96b6deeec9d0ff6ea962
Norman Wiechmann <norman.wiechmann@aperto.de>
2009-01-09 17:18:28 +0000

    fixed exception while determine current locale without magnolia context, eg. of bypassed requests

--- src/main/java/com/aperto/magkit/i18n/MgnlLocaleResolver.java
+++ src/main/java/com/aperto/magkit/i18n/MgnlLocaleResolver.java
@@ -1,26 +1,33 @@
 package com.aperto.magkit.i18n;
 
-import info.magnolia.cms.i18n.I18nContentSupport;
-import info.magnolia.cms.i18n.I18nContentSupportFactory;
-import org.apache.log4j.Logger;
-import org.springframework.web.servlet.i18n.AbstractLocaleResolver;
+import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Locale;
+
+import info.magnolia.cms.i18n.I18nContentSupport;
+import info.magnolia.cms.i18n.I18nContentSupportFactory;
+import info.magnolia.context.MgnlContext;
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.i18n.AbstractLocaleResolver;
 
 /**
- * Implementiation of the @see{AbstractLocaleResolver}.
+ * Implementiation of the {@link AbstractLocaleResolver} that determines the current locale used for rendering content
+ * by magnolia.
  *
- * @author frank.sommer (21.08.2008)
+ * @author Frank Sommer, Aperto AG (2008-08-21)
  */
 public class MgnlLocaleResolver extends AbstractLocaleResolver {
     private static final Logger LOGGER = Logger.getLogger(MgnlLocaleResolver.class);
 
     public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+        Locale locale;
         I18nContentSupport i18nSupport = I18nContentSupportFactory.getI18nSupport();
-        Locale locale = super.getDefaultLocale();
-        if (i18nSupport != null) {
+        // To get a locale from i18n support implementation a magnolia context is required. Otherwise the getLocale()
+        // method will fail with an exception.
+        if (i18nSupport != null && MgnlContext.hasInstance()) {
             locale = i18nSupport.getLocale();
+        } else {
+            locale = super.getDefaultLocale();
         }
         LOGGER.debug("Locale resolved to: " + locale.toString());
         return locale;
