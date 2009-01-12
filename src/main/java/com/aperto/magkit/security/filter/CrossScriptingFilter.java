c6198e3adef366ff1b2edd52f665ce7498378d6f
Mayo Fragoso <mayo.fragoso@aperto.de>
2009-01-12 17:10:20 +0000

    added uriFilterList restriction as init-param

--- src/main/java/com/aperto/magkit/security/filter/CrossScriptingFilter.java
+++ src/main/java/com/aperto/magkit/security/filter/CrossScriptingFilter.java
@@ -1,6 +1,8 @@
 package com.aperto.magkit.security.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,6 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
 
 /**
  * This filter uses a request wrapper which deactivates javascript tags inculed within http parameters or header by encoding them.
@@ -19,12 +23,17 @@ import javax.servlet.http.HttpServletRequest;
 public class CrossScriptingFilter implements Filter {
 
     private FilterConfig _filterConfig;
-
+    private List<String> _uriFilterList;
+    
     /**
      * {@inheritDoc}
      */
     public void init(FilterConfig filterConfig) throws ServletException {
         _filterConfig = filterConfig;
+        String initParameter = filterConfig.getInitParameter("uriFilterList");
+        if (initParameter != null) {
+            _uriFilterList = Arrays.asList(StringUtils.stripAll(StringUtils.split(initParameter, ",")));
+        }
     }
 
     /**
@@ -38,6 +47,11 @@ public class CrossScriptingFilter implements Filter {
      * Wraps the request and execute it filtering javascript from http parameters an header.
      */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(new CrossScriptingRequestWrapper((HttpServletRequest) request), response);
+        String requestUri = ((HttpServletRequest) request).getRequestURI();
+        if (_uriFilterList != null && _uriFilterList.contains(requestUri)) {
+            chain.doFilter(new CrossScriptingRequestWrapper((HttpServletRequest) request), response);
+        } else {
+            chain.doFilter(request, response);
+        }
     }
 }
