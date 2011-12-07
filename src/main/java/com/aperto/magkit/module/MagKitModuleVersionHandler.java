b6c7a967d2afb0c7731caecb6adde348f8a6be7a
Frank Sommer <frank.sommer@aperto.de>
2011-12-07 13:34:54 +0000

    MGKT-138: update bypass config

--- src/main/java/com/aperto/magkit/module/MagKitModuleVersionHandler.java
+++ src/main/java/com/aperto/magkit/module/MagKitModuleVersionHandler.java
@@ -2,7 +2,6 @@ package com.aperto.magkit.module;
 
 import com.aperto.webkit.utils.ExceptionEater;
 import info.magnolia.cms.core.ItemType;
-import info.magnolia.module.DefaultModuleVersionHandler;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.*;
 import info.magnolia.module.model.Version;
@@ -19,15 +18,12 @@ import static info.magnolia.cms.beans.config.ContentRepository.CONFIG;
  * The MagKitModuleVersionHandler for the MagKit module.
  *
  * @author frank.sommer
- *         Date: 03.04.2008
+ * @since 03.04.2008
  */
-public class MagKitModuleVersionHandler extends DefaultModuleVersionHandler {
+public class MagKitModuleVersionHandler extends BootstrapModuleVersionHandler {
     private static final String PATH_FILTER = "/server/filters";
     private static final String PATH_FILTER_VALIDATOR = PATH_FILTER + "/validator";
     private static final String PATH_I18N = "/server/i18n/content";
-    private static final String PATH_CACHE_EXCLUDE = "/modules/cache/config/configurations/default/cachePolicy/voters/urls/excludes";
-    private static final String PATH_CACHE_CAPTCHA = PATH_CACHE_EXCLUDE + "/captcha";
-    private static final String PATH_CACHE_DEBUG = PATH_CACHE_EXCLUDE + "/debug";
 
     private static final String PROPERTY_KEY = "environment";
     private static final String PROPERTY_VALUE_PRODUCTION_ENVIRONMENT = "production";
@@ -53,21 +49,9 @@ public class MagKitModuleVersionHandler extends DefaultModuleVersionHandler {
     });
 
     private final Task _addBypassFor404 = new NodeExistsDelegateTask(
-        "Check 404 bypass",
-        "Check 404 bypass in server config.",
-        CONFIG,
-        PATH_FILTER + "/bypasses/404",
-        null,
+        "Check 404 bypass", "Check 404 bypass in server config.",
+        CONFIG, PATH_FILTER + "/bypasses/404", null,
         new AddFilterBypassTask(PATH_FILTER, "404", URIStartsWithVoter.class, "/docroot/magkit")
-    );
-
-    private final Task _addBypassForStatus = new NodeExistsDelegateTask(
-        "Check status bypass",
-        "Check status bypass in server config.",
-        CONFIG,
-        PATH_FILTER + "/bypasses/status",
-        null,
-        new AddFilterBypassTask(PATH_FILTER, "status", URIStartsWithVoter.class, "/status")
     );
 
     private final Task _addBypassForDebugSuite = new NodeExistsDelegateTask(
@@ -76,21 +60,10 @@ public class MagKitModuleVersionHandler extends DefaultModuleVersionHandler {
         new AddFilterBypassTask(PATH_FILTER, "debug", URIStartsWithVoter.class, "/debug/")
     );
 
-    private final Task _addCacheConfig = new ArrayDelegateTask("Captcha config", "Add the cache config for captcha.",
-            new CreateNodeTask("captcha", "Create cache config node.", CONFIG, PATH_CACHE_EXCLUDE, "captcha", ItemType.CONTENTNODE.getSystemName()),
-            new SetPropertyTask(CONFIG, PATH_CACHE_CAPTCHA, "pattern", "/service/captcha/"),
-            new SetPropertyTask(CONFIG, PATH_CACHE_CAPTCHA, "class", "info.magnolia.voting.voters.URIStartsWithVoter"),
-            new CreateNodeTask("debug", "Create cache config node.", CONFIG, PATH_CACHE_EXCLUDE, "debug", ItemType.CONTENTNODE.getSystemName()),
-            new SetPropertyTask(CONFIG, PATH_CACHE_DEBUG, "pattern", "/debug/"),
-            new SetPropertyTask(CONFIG, PATH_CACHE_DEBUG, "class", "info.magnolia.voting.voters.URIStartsWithVoter"));
-
-    private final Task _checkCacheConfig = new NodeExistsDelegateTask(
-        "Check cache config",
-        "Check cache config.",
-        CONFIG,
-        PATH_CACHE_CAPTCHA,
-        null,
-        _addCacheConfig
+    private final Task _addSpringByPass = new NodeExistsDelegateTask(
+        "Check spring bypass", "Check spring bypass in server config.",
+        CONFIG, PATH_FILTER + "/cms/bypasses/spring", null,
+        new AddFilterBypassTask(PATH_FILTER + "/cms", "spring", URIStartsWithVoter.class, "/service/")
     );
 
     private final Task _bootstrapApertoTools = new BootstrapResourcesTask("Aperto Tools", "Bootstraps the Aperto Tools Menu.") {
@@ -116,12 +89,6 @@ public class MagKitModuleVersionHandler extends DefaultModuleVersionHandler {
             return returnValue;
         }
     };
-
-    /**
-     * this is used to bootstrap the new module-specific templates, dialogs ... .
-     * bootstraps everything from "mgnl-bootstrap" folder.
-     */
-    private final Task _bootstrapModuleConfigTask = new ModuleBootstrapTask();
 
     /**
      * Constructor for adding update builder.
@@ -169,20 +136,17 @@ public class MagKitModuleVersionHandler extends DefaultModuleVersionHandler {
         tasks.add(_addValidatorFilterBypassTask);
         tasks.add(_setI18nContentSupportTask);
         tasks.add(_addBypassFor404);
-        tasks.add(_addBypassForStatus);
         tasks.add(_addBypassForDebugSuite);
-        tasks.add(_checkCacheConfig);
+        tasks.add(_addSpringByPass);
         return tasks;
     }
 
     @Override
     protected List<Task> getDefaultUpdateTasks(Version forVersion) {
         List<Task> updateTasks = super.getDefaultUpdateTasks(forVersion);
-        updateTasks.add(_bootstrapModuleConfigTask);
         updateTasks.add(_addBypassFor404);
-        updateTasks.add(_addBypassForStatus);
         updateTasks.add(_addBypassForDebugSuite);
-        updateTasks.add(_checkCacheConfig);
+        updateTasks.add(_addSpringByPass);
         return updateTasks;
     }
 }
