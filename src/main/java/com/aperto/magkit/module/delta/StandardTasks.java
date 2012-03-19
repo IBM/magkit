56dd4b35812ac442b5e6b02d9a226d3c9c2865c3
Frank Sommer <frank.sommer@aperto.de>
2012-03-19 12:15:56 +0000

    MGKT-149: add stk favicon task

--- src/main/java/com/aperto/magkit/module/delta/StandardTasks.java
+++ src/main/java/com/aperto/magkit/module/delta/StandardTasks.java
@@ -1,8 +1,8 @@
 package com.aperto.magkit.module.delta;
 
+import info.magnolia.cms.beans.config.DefaultVirtualURIMapping;
 import info.magnolia.module.delta.Task;
 
-import static com.aperto.magkit.module.delta.CreateConfigNodeTreeTask.*;
 import static com.aperto.magkit.nodebuilder.NodeOperationFactory.addOrGetNode;
 import static com.aperto.magkit.nodebuilder.NodeOperationFactory.addOrSetProperty;
 import static com.aperto.magkit.nodebuilder.task.NodeBuilderTaskFactory.selectModuleConfig;
@@ -13,32 +13,34 @@ import static com.aperto.magkit.nodebuilder.task.NodeBuilderTaskFactory.selectMo
  * @author Norman Wiechmann (Aperto AG)
  */
 public class StandardTasks {
-
-    private static final String URI_MAPPING = "virtualURIMapping";
+    public static final String URI_MAPPING = "virtualURIMapping";
 
     /**
      * Creates an menu for the given module with templates, paragraphs and dialogs links.
      */
     public static Task createAdminInterfaceMenu(final String moduleName, final String moduleDisplayName) {
-        return selectConfig("AdminInterface Menu",
-                "Create " + moduleDisplayName + " menue items within module adminInterface.",
-                "/modules/adminInterface/config/menu",
-                createNode(moduleName,
-                        setProperty("icon", "/.resources/icons/24/gears.gif"),
-                        setProperty("onclick", "MgnlAdminCentral.showTree('config', '/modules/" + moduleName + "')"),
-                        setProperty("label", moduleDisplayName),
-                        createNode("templates",
-                                setProperty("icon", "/.resources/icons/16/dot.gif"),
-                                setProperty("onclick", "MgnlAdminCentral.showTree('config','/modules/" + moduleName + "/templates')"),
-                                setProperty("label", "menu.config.templates")),
-                        createNode("paragraphs",
-                                setProperty("icon", "/.resources/icons/16/dot.gif"),
-                                setProperty("onclick", "MgnlAdminCentral.showTree('config','/modules/" + moduleName + "/paragraphs')"),
-                                setProperty("label", "menu.config.paragraphs")),
-                        createNode("dialogs",
-                                setProperty("icon", "/.resources/icons/16/dot.gif"),
-                                setProperty("onclick", "MgnlAdminCentral.showTree('config','/modules/" + moduleName + "/dialogs')"),
-                                setProperty("label", "menu.config.dialogs"))));
+        return selectModuleConfig("Module Menu", "Create " + moduleDisplayName + " menue items within module adminInterface.", "adminInterface",
+            addOrGetNode("config/menu" + moduleName).then(
+                addOrSetProperty("icon", "/.resources/icons/24/gears.gif"),
+                addOrSetProperty("onclick", "MgnlAdminCentral.showTree('config', '/modules/" + moduleName + "')"),
+                addOrSetProperty("label", moduleDisplayName),
+                addOrGetNode("templates").then(
+                    addOrSetProperty("icon", "/.resources/icons/16/dot.gif"),
+                    addOrSetProperty("onclick", "MgnlAdminCentral.showTree('config','/modules/" + moduleName + "/templates')"),
+                    addOrSetProperty("label", "menu.config.templates")
+                ),
+                addOrGetNode("paragraphs").then(
+                    addOrSetProperty("icon", "/.resources/icons/16/dot.gif"),
+                    addOrSetProperty("onclick", "MgnlAdminCentral.showTree('config','/modules/" + moduleName + "/paragraphs')"),
+                    addOrSetProperty("label", "menu.config.paragraphs")
+                ),
+                addOrGetNode("dialogs").then(
+                    addOrSetProperty("icon", "/.resources/icons/16/dot.gif"),
+                    addOrSetProperty("onclick", "MgnlAdminCentral.showTree('config','/modules/" + moduleName + "/dialogs')"),
+                    addOrSetProperty("label", "menu.config.dialogs")
+                )
+            )
+        );
     }
 
     /**
@@ -46,11 +48,11 @@ public class StandardTasks {
      */
     public static Task virtualUriMappingOfRobotsTxt(final String moduleName) {
         return selectModuleConfig("Virtual UriMapping", "Add virtual URI mapping for robots.txt.", moduleName,
-                addOrGetNode(URI_MAPPING).then(
-                        addOrGetNode("robots").then(
-                                addOrSetProperty("class", "info.magnolia.cms.beans.config.DefaultVirtualURIMapping"),
-                                addOrSetProperty("fromURI", "/robots.txt"),
-                                addOrSetProperty("toURI", "forward:/docroot/" + moduleName + "/robots.txt"))));
+            addOrGetNode(URI_MAPPING).then(
+                addOrGetNode("robots").then(
+                    addOrSetProperty("class", DefaultVirtualURIMapping.class.getName()),
+                    addOrSetProperty("fromURI", "/robots.txt"),
+                    addOrSetProperty("toURI", "forward:/docroot/" + moduleName + "/robots.txt"))));
     }
 
     /**
@@ -58,11 +60,11 @@ public class StandardTasks {
      */
     public static Task virtualUriMappingOfFavicon(final String moduleName) {
         return selectModuleConfig("Virtual UriMapping", "Add virtual URI mapping for favicon.", moduleName,
-                addOrGetNode(URI_MAPPING).then(
-                        addOrGetNode("favicon").then(
-                                addOrSetProperty("class", "info.magnolia.cms.beans.config.DefaultVirtualURIMapping"),
-                                addOrSetProperty("fromURI", "/favicon.ico"),
-                                addOrSetProperty("toURI", "forward:/docroot/" + moduleName + "/favicon.ico"))));
+            addOrGetNode(URI_MAPPING).then(
+                addOrGetNode("favicon").then(
+                    addOrSetProperty("class", DefaultVirtualURIMapping.class.getName()),
+                    addOrSetProperty("fromURI", "/favicon.ico"),
+                    addOrSetProperty("toURI", "forward:/docroot/" + moduleName + "/favicon.ico"))));
     }
 
     protected StandardTasks() {
