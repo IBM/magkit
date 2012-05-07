72e20af09a9f9c25c2d526a06b1d70bd5d6f7e79
Frank Sommer <frank.sommer@aperto.de>
2012-05-07 13:12:53 +0000

    MGKT-150: remove validator

--- src/main/java/com/aperto/magkit/module/MagKitModuleVersionHandler.java
+++ src/main/java/com/aperto/magkit/module/MagKitModuleVersionHandler.java
@@ -1,16 +1,15 @@
 package com.aperto.magkit.module;
 
 import info.magnolia.module.InstallContext;
-import info.magnolia.module.delta.*;
+import info.magnolia.module.delta.NodeExistsDelegateTask;
+import info.magnolia.module.delta.Task;
 import info.magnolia.module.model.Version;
 import info.magnolia.setup.initial.AddFilterBypassTask;
-import info.magnolia.voting.voters.OnAdminVoter;
 import info.magnolia.voting.voters.URIStartsWithVoter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static info.magnolia.cms.core.MgnlNodeType.NT_CONTENT;
 import static info.magnolia.repository.RepositoryConstants.CONFIG;
 
 /**
@@ -21,21 +20,6 @@ import static info.magnolia.repository.RepositoryConstants.CONFIG;
  */
 public class MagKitModuleVersionHandler extends BootstrapModuleVersionHandler {
     private static final String PATH_FILTER = "/server/filters";
-    private static final String PATH_FILTER_VALIDATOR = PATH_FILTER + "/validator";
-
-    private final Task _addValidatorFilterTask = new ArrayDelegateTask("Filter", "Add the Validator filter.",
-        new CreateNodeTask("Validator-Filter", "Create Validator filter node", CONFIG, PATH_FILTER, "validator", NT_CONTENT),
-        new SetPropertyTask(CONFIG, PATH_FILTER_VALIDATOR, "class", "com.aperto.magkit.filter.HtmlValidatorFilter"),
-        new SetPropertyTask(CONFIG, PATH_FILTER_VALIDATOR, "enabled", "true"),
-        new SetPropertyTask(CONFIG, PATH_FILTER_VALIDATOR, "w3cValidatorCheckUrl", "http://validator.aperto.de/w3c-markup-validator/check"),
-        new SetPropertyTask(CONFIG, PATH_FILTER_VALIDATOR, "timeOut", "15000"),
-        new FilterOrderingTask("validator", new String[]{"contentType", "uriSecurity", "gzip"})
-    );
-
-    private final Task _addValidatorFilterBypassTask = new ArrayDelegateTask("Filter", "Add the bypass for validator filter.",
-        new AddFilterBypassTask(PATH_FILTER_VALIDATOR, "isAdmin", OnAdminVoter.class, ""),
-        new SetPropertyTask(CONFIG, PATH_FILTER_VALIDATOR + "/bypasses/isAdmin", "not", "true")
-    );
 
     private final Task _addBypassFor404 = new NodeExistsDelegateTask(
         "Check 404 bypass", "Check 404 bypass in server config.",
@@ -64,8 +48,6 @@ public class MagKitModuleVersionHandler extends BootstrapModuleVersionHandler {
     @Override
     protected List<Task> getExtraInstallTasks(InstallContext installContext) {
         final List<Task> tasks = new ArrayList<Task>();
-        tasks.add(_addValidatorFilterTask);
-        tasks.add(_addValidatorFilterBypassTask);
         tasks.add(_addBypassFor404);
         tasks.add(_addBypassForDebugSuite);
         tasks.add(_addSpringByPass);
