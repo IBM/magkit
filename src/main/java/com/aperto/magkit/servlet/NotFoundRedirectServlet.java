d1154b9baab987a5aadf5b48ac640a83aee2d660
Diana Racho <diana.racho@aperto.de>
2012-08-20 06:47:08 +0000

    MGKT-155: add 404 redirect servlet

---
+++ src/main/java/com/aperto/magkit/servlet/NotFoundRedirectServlet.java
@@ -0,0 +1,84 @@
+package com.aperto.magkit.servlet;
+
+import info.magnolia.cms.i18n.I18nContentSupport;
+import info.magnolia.cms.util.RequestDispatchUtil;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static org.apache.commons.lang.StringUtils.*;
+
+/**
+ * 404 error redirect servlet.
+ * Configurate handle of error pages in init parameters:
+ * - default
+ * - language
+ * - tenant_language
+ *
+ * @author diana.racho (Aperto AG)
+ */
+public class NotFoundRedirectServlet extends HttpServlet {
+    private static final long serialVersionUID = -2569111666576917867L;
+
+    @Inject
+    private I18nContentSupport _i18nContentSupport;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String contextPath = request.getContextPath();
+        String originUri = (String) request.getAttribute("javax.servlet.forward.request_uri");
+        String part1 = EMPTY;
+        String part2 = EMPTY;
+        if (isNotBlank(originUri)) {
+            int startLevel = 0;
+            if (isNotBlank(contextPath) && originUri.startsWith(contextPath)) {
+                startLevel++;
+            }
+            String[] parts = split(originUri, '/');
+
+            if (parts.length > startLevel) {
+                part1 = parts[startLevel];
+            }
+            startLevel++;
+            if (parts.length > startLevel) {
+                part2 = parts[startLevel];
+            }
+        }
+        String handle = getRedirectHandle(part1, part2);
+        response.setStatus(SC_NOT_FOUND);
+        if (isNotBlank(handle)) {
+            RequestDispatchUtil.dispatch("forward:" + handle, request, response);
+        } else {
+            PrintWriter writer = response.getWriter();
+            writer.write("404 redirect handles are not configurated.");
+        }
+    }
+
+    private String getRedirectHandle(String part1, String part2) {
+        String handle = getInitParameter(part1 + "_" + part2);
+        if (isBlank(handle)) {
+            handle = getInitParameter(part1);
+            if (isBlank(handle)) {
+                handle = getInitParameter(getI18nContentSupport().getLocale().getLanguage());
+                if (isBlank(handle)) {
+                    handle = getInitParameter("default");
+                }
+            }
+        }
+        return handle;
+    }
+
+    public I18nContentSupport getI18nContentSupport() {
+        return _i18nContentSupport;
+    }
+
+    public void setI18nContentSupport(I18nContentSupport i18nContentSupport) {
+        _i18nContentSupport = i18nContentSupport;
+    }
+}
