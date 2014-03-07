a04c569d8a1fc45226a4f3f006ff7e996ad73047
Frank Sommer <frank.sommer@aperto.de>
2014-03-07 14:30:47 +0000

    enhance message manager

--- src/main/java/com/aperto/magkit/i18n/ExtendedMessagesManager.java
+++ src/main/java/com/aperto/magkit/i18n/ExtendedMessagesManager.java
@@ -17,10 +17,12 @@ import java.util.Locale;
 import java.util.Set;
 
 import static com.aperto.magkit.utils.PropertyUtils.retrieveOrderedMultiSelectValues;
+import static info.magnolia.jcr.util.PropertyUtil.getString;
 import static info.magnolia.repository.RepositoryConstants.CONFIG;
 
 /**
- * Delivers all configurated basenames under /server/i18n/content/i18nBasenames for all basenames which matchs pattern PATTERN_MESSAGES and PATTERN_DIALOGS.
+ * Delivers all configurated basenames under {@link #SERVER_I18N_BASENAMES} for all basenames which matches
+ * configured pattern {@link #PN_PROJECT_PATTERN} below {@link #SERVER_I18N_CONTENT}.
  * The ExtendedMessagesManager class must be specified in magnolia.properties under info.magnolia.cms.i18n.MessagesManager.
  *
  * @author Achim.Herbertz, diana.racho, frank.sommer
@@ -28,8 +30,9 @@ import static info.magnolia.repository.RepositoryConstants.CONFIG;
 public class ExtendedMessagesManager extends DefaultMessagesManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExtendedMessagesManager.class);
 
-    private static final String PATTERN_MESSAGES = "com.aperto.+.messages";
+    private static final String DEFAULT_PATTERN = "com.aperto.+.messages";
     public static final String SERVER_I18N_CONTENT = "/server/i18n/content";
+    public static final String PN_PROJECT_PATTERN = "basenamePattern";
     public static final String CN_BASENAMES = "i18nBasenames";
     public static final String SERVER_I18N_BASENAMES = SERVER_I18N_CONTENT + "/" + CN_BASENAMES;
 
@@ -76,7 +79,18 @@ public class ExtendedMessagesManager extends DefaultMessagesManager {
     }
 
     protected boolean isProjectBasename(String basename) {
-        return basename.matches(PATTERN_MESSAGES);
+        boolean projectBasename = false;
+        try {
+            Session jcrSession = _systemContext.getJCRSession(CONFIG);
+            if (jcrSession.nodeExists(SERVER_I18N_CONTENT)) {
+                Node i18nNode = jcrSession.getNode(SERVER_I18N_CONTENT);
+                String pattern = getString(i18nNode, PN_PROJECT_PATTERN, DEFAULT_PATTERN);
+                projectBasename = basename.matches(pattern);
+            }
+        } catch (RepositoryException e) {
+            LOGGER.error("Error retrieving i18n project pattern from i18n config.", e);
+        }
+        return projectBasename;
     }
 
     protected Set<String> retrieveBasenames() {
