611e7a9759b416516fae62b66493b5d7d6eba452
Frank Sommer <frank.sommer@aperto.de>
2014-12-03 09:26:56 +0000

    refactor script install task

--- src/main/java/com/aperto/magkit/module/delta/ScriptInstallTask.java
+++ src/main/java/com/aperto/magkit/module/delta/ScriptInstallTask.java
@@ -1,62 +1,83 @@
 package com.aperto.magkit.module.delta;
 
+import info.magnolia.cms.util.ClasspathResourcesUtil;
 import info.magnolia.context.MgnlContext;
-import info.magnolia.jcr.util.NodeTypes;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.AbstractTask;
 import info.magnolia.module.delta.TaskExecutionException;
-import info.magnolia.objectfactory.Components;
-import info.magnolia.templating.functions.TemplatingFunctions;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 
-import static info.magnolia.jcr.util.PropertyUtil.setProperty;
-import static java.lang.Boolean.TRUE;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static info.magnolia.cms.core.Path.getValidatedLabel;
+import static info.magnolia.jcr.util.NodeTypes.Content;
+import static info.magnolia.jcr.util.NodeTypes.Folder;
+import static info.magnolia.jcr.util.NodeUtil.createPath;
+import static org.apache.commons.io.FilenameUtils.getBaseName;
+import static org.apache.commons.io.IOUtils.closeQuietly;
+import static org.apache.commons.lang.CharEncoding.UTF_8;
+import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.apache.commons.lang.StringUtils.removeEnd;
 
 /**
- * Install task installing scripts.
- * @author Aperto AG
+ * Install task for load groovy scripts in scripts workspace.
+ * <pre>
+ * new ScriptInstallTask("/täst.groovy"); = create groovy script in root
+ * new ScriptInstallTask("/täst.groovy", "/folder"); = create groovy script in folder
+ * new ScriptInstallTask("/täst.groovy", "/folder2", false); = create groovy file (e.g. class) in folder2
+ * </pre>
+ *
+ * @author diana.racho
+ * @author frank.sommer
+ * @since version 3.1.2
  */
 public class ScriptInstallTask extends AbstractTask {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ScriptInstallTask.class);
-
     private static final String SCRIPT_WORKSPACE_NAME = "scripts";
 
     private String _resource;
-    private final TemplatingFunctions _templatingFunctions = Components.getComponent(TemplatingFunctions.class);
+    private String _basePath;
+    private boolean _script;
 
     public ScriptInstallTask(final String resource) {
-        this("Install scripts", "Install or update scripts.", resource);
+        this(resource, "/");
     }
 
-    public ScriptInstallTask(final String taskName, final String taskDescription, final String resource) {
+    public ScriptInstallTask(final String resource, final String basePath) {
+        this(resource, basePath, true);
+    }
+
+    public ScriptInstallTask(final String resource, final String basePath, final boolean script) {
+        this("Install script", "Install script (if not already there): " + resource + " below " + basePath, resource, basePath, script);
+    }
+
+    protected ScriptInstallTask(final String taskName, final String taskDescription, final String resource, final String basePath, final boolean script) {
         super(taskName, taskDescription);
         _resource = resource;
+        _basePath = basePath;
+        _script = script;
     }
 
     @Override
     public void execute(final InstallContext installContext) throws TaskExecutionException {
-        String nodeName = _resource.substring(StringUtils.lastIndexOf(_resource, '/') + 1, _resource.indexOf('.'));
+        String validNodeName = getValidatedLabel(getBaseName(_resource));
         try {
             final Session scriptSession = MgnlContext.getJCRSession(SCRIPT_WORKSPACE_NAME);
-            Node node = _templatingFunctions.nodeByPath(nodeName, SCRIPT_WORKSPACE_NAME);
-            if (node == null || !node.isNodeType(NodeTypes.Content.NAME)) {
-                node = scriptSession.getRootNode().addNode(nodeName, NodeTypes.Content.NAME);
-                setProperty(node, "script", TRUE);
-                setProperty(node, "text", getText());
+            String nodePath = removeEnd(_basePath, "/") + "/" + validNodeName;
+            if (!scriptSession.itemExists(nodePath)) {
+                Node baseNode;
+                if (scriptSession.itemExists(_basePath)) {
+                    baseNode = scriptSession.getNode(_basePath);
+                } else {
+                    baseNode = createPath(scriptSession.getRootNode(), _basePath, Folder.NAME);
+                }
+                Node node = baseNode.addNode(validNodeName, Content.NAME);
+                node.setProperty("script", _script);
+                node.setProperty("text", getText());
             }
-            scriptSession.save();
         } catch (RepositoryException e) {
             throw new TaskExecutionException("Failed to add script with " + e.getMessage(), e);
         } catch (IOException e) {
@@ -66,16 +87,11 @@ public class ScriptInstallTask extends AbstractTask {
 
     protected String getText() throws IOException {
         String text = EMPTY;
-        InputStream is = ScriptInstallTask.class.getResourceAsStream(_resource);
-        StringWriter sw = new StringWriter();
+        InputStream is = ClasspathResourcesUtil.getStream(_resource);
         try {
-            IOUtils.copy(is, sw);
-            text = sw.getBuffer().toString();
-        } catch (IOException e) {
-            LOGGER.error("Failed to read script.", e);
+            text = IOUtils.toString(is, UTF_8);
         } finally {
-            is.close();
-            sw.close();
+            closeQuietly(is);
         }
         return text;
     }
