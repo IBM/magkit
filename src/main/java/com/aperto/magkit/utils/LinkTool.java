0bdf9afa7e394ecfe39aaa446147990cb1ffc20c
Frank Sommer <frank.sommer@aperto.de>
2015-01-12 14:16:47 +0000

    add some more constants

--- src/main/java/com/aperto/magkit/utils/LinkTool.java
+++ src/main/java/com/aperto/magkit/utils/LinkTool.java
@@ -3,6 +3,7 @@ package com.aperto.magkit.utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.apache.commons.lang.StringUtils.isNotBlank;
 import static org.apache.commons.lang.StringUtils.isNotEmpty;
 import static org.apache.commons.lang.StringUtils.startsWithIgnoreCase;
 
@@ -13,8 +14,14 @@ import static org.apache.commons.lang.StringUtils.startsWithIgnoreCase;
  */
 public final class LinkTool {
     public static final Pattern UUID_PATTERN = Pattern.compile("^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$");
-    public static final String HTTP_PREFIX = "http://";
-    public static final String HTTPS_PREFIX = "https://";
+
+    public static final String HTTP = "http";
+    public static final String HTTPS = "https";
+    public static final int HTTP_PORT = 80;
+    public static final int HTTPS_PORT = 443;
+    public static final String PROTOCOL_DELIMITER = "://";
+    public static final String HTTP_PREFIX = HTTP + PROTOCOL_DELIMITER;
+    public static final String HTTPS_PREFIX = HTTPS + PROTOCOL_DELIMITER;
 
     /**
      * Checks if the given link is a uuid.
@@ -49,7 +56,7 @@ public final class LinkTool {
      * @return true for path links
      */
     public static boolean isPath(final String linkValue) {
-        return startsWithIgnoreCase(linkValue, "/");
+        return isNotBlank(linkValue) && linkValue.startsWith("/");
     }
 
     private LinkTool() {
