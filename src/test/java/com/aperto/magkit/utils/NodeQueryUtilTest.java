db41999c363b09d60924771114e8ac87132d7489
Frank Sommer <frank.sommer@aperto.de>
2015-02-10 08:47:16 +0000

    refactor test

--- src/test/java/com/aperto/magkit/utils/NodeQueryUtilTest.java
+++ src/test/java/com/aperto/magkit/utils/NodeQueryUtilTest.java
@@ -6,20 +6,16 @@ import org.junit.Test;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
-import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
 
 import static com.aperto.magkit.mockito.ContextMockUtils.cleanContext;
 import static com.aperto.magkit.mockito.ContextMockUtils.mockWebContext;
 import static com.aperto.magkit.mockito.WebContextStubbingOperation.stubJcrSession;
-import static com.aperto.magkit.mockito.jcr.QueryManagerStubbingOperation.stubbQuery;
 import static com.aperto.magkit.mockito.jcr.QueryMockUtils.mockQueryManager;
-import static com.aperto.magkit.mockito.jcr.QueryStubbingOperation.stubbResult;
 import static com.aperto.magkit.utils.NodeQueryUtil.getComponentsWithTemplate;
 import static info.magnolia.repository.RepositoryConstants.WEBSITE;
 import static javax.jcr.query.Query.XPATH;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
 /**
@@ -35,46 +31,26 @@ public class NodeQueryUtilTest {
 
     @Test
     public void testReplaceStringBuilderWithXpathBuilder() throws Exception {
-        final StringBuilder statement = new StringBuilder();
-        statement.append("/jcr:root");
-        statement.append(TEST_NODE_PATH);
-        statement.append("//element(*," + TEST_TYPE + ")");
-        statement.append("[@mgnl:template='").append(TEST_TPL_NAME).append("']");
-        statement.append(TEST_CUSTOM_XPATH_EXPRESSION);
-
         final ConstraintBuilder builder = new ConstraintBuilder().addTplNameConstraint(TEST_TPL_NAME);
-        assertEquals(statement.toString(), new XpathBuilder().path(TEST_NODE_PATH).type(TEST_TYPE).property(builder).append(TEST_CUSTOM_XPATH_EXPRESSION).build());
+        assertEquals("/jcr:root" + TEST_NODE_PATH + "//element(*," + TEST_TYPE + ")" + "[@mgnl:template='" + TEST_TPL_NAME + "']" + TEST_CUSTOM_XPATH_EXPRESSION, new XpathBuilder().path(TEST_NODE_PATH).type(TEST_TYPE).property(builder).append(TEST_CUSTOM_XPATH_EXPRESSION).build());
     }
 
     @Test
     public void testReplaceStringBuilderWithXpathBuilderWithoutPath() throws Exception {
-        final StringBuilder statement = new StringBuilder();
-        statement.append("/jcr:root");
-        statement.append("//element(*," + TEST_TYPE + ")");
-        statement.append("[@mgnl:template='").append(TEST_TPL_NAME).append("']");
-        statement.append(TEST_CUSTOM_XPATH_EXPRESSION);
-
         final ConstraintBuilder builder = new ConstraintBuilder().addTplNameConstraint(TEST_TPL_NAME);
-        assertEquals(statement.toString(), new XpathBuilder().type(TEST_TYPE).property(builder).append(TEST_CUSTOM_XPATH_EXPRESSION).build());
+        assertEquals("/jcr:root" + "//element(*," + TEST_TYPE + ")" + "[@mgnl:template='" + TEST_TPL_NAME + "']" + TEST_CUSTOM_XPATH_EXPRESSION, new XpathBuilder().type(TEST_TYPE).property(builder).append(TEST_CUSTOM_XPATH_EXPRESSION).build());
     }
 
     @Test
     public void testReplaceStringBuilderWithXpathBuilderWithoutCustomExpression() throws Exception {
-        final StringBuilder statement = new StringBuilder();
-        statement.append("/jcr:root");
-        statement.append(TEST_NODE_PATH);
-        statement.append("//element(*," + TEST_TYPE + ")");
-        statement.append("[@mgnl:template='").append(TEST_TPL_NAME).append("']");
-
         final ConstraintBuilder builder = new ConstraintBuilder().addTplNameConstraint(TEST_TPL_NAME);
-        assertEquals(statement.toString(), new XpathBuilder().path(TEST_NODE_PATH).type(TEST_TYPE).property(builder).append("").build());
+        assertEquals("/jcr:root" + TEST_NODE_PATH + "//element(*," + TEST_TYPE + ")" + "[@mgnl:template='" + TEST_TPL_NAME + "']", new XpathBuilder().path(TEST_NODE_PATH).type(TEST_TYPE).property(builder).append("").build());
     }
 
     @Test
     public void testComponentQueryWithoutRoot() throws RepositoryException {
         getComponentsWithTemplate(TEST_TPL_NAME, (Node) null, null);
         verify(_queryManager).createQuery("/jcr:root//element(*,mgnl:component)[@mgnl:template='test-case:pages/templateName'] order by @jcr:primaryType", XPATH);
-
     }
 
     @Test
@@ -99,9 +75,7 @@ public class NodeQueryUtilTest {
         mockWebContext(stubJcrSession(WEBSITE));
 
         // setup mock QueryManager
-        _queryManager = mockQueryManager(WEBSITE,
-                stubbQuery(Query.XPATH, anyString(), stubbResult())
-        );
+        _queryManager = mockQueryManager(WEBSITE);
     }
 
     @After
