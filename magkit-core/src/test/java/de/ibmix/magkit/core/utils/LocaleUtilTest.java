989101950180fa5e35d7018afdf91a6cb6c1dd00
wolf.bubenik <wolf.bubenik@aperto.com>
2024-01-04 15:03:22 +0100

    issue 16: add licence info

--- magkit-core/src/test/java/de/ibmix/magkit/core/utils/LocaleUtilTest.java
+++ magkit-core/src/test/java/de/ibmix/magkit/core/utils/LocaleUtilTest.java
@@ -1,5 +1,26 @@
 package de.ibmix.magkit.core.utils;
 
+/*-
+ * #%L
+ * IBM iX Magnolia Kit
+ * %%
+ * Copyright (C) 2023 IBM iX
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import de.ibmix.magkit.test.cms.context.ContextMockUtils;
 import de.ibmix.magkit.test.cms.context.I18nContentSupportStubbingOperation;
 import de.ibmix.magkit.test.cms.site.SiteMockUtils;
@@ -17,6 +38,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Testing LocaleUtil.
+ *
+ * @author wolf.bubenik.ibmic.de
+ * @since 2024-01-03
+ */
 public class LocaleUtilTest {
 
     private Site _defaultSite;
