726362fd72ae3fb48c1e8f2e2b823c1e99156532
wolf.bubenik <wolf.bubenik@aperto.com>
2024-03-26 13:23:12 +0100

    make some date tests more flexible

--- magkit-query/src/test/java/de/ibmix/magkit/query/sql2/Sql2ConditionTest.java
+++ magkit-query/src/test/java/de/ibmix/magkit/query/sql2/Sql2ConditionTest.java
@@ -58,14 +58,14 @@ public class Sql2ConditionTest {
     @Test
     public void dateCreatedBefore() {
         assertThat(Sql2.Condition.Date.createdBefore(null).asString(), is(""));
-        assertTrue(Sql2.Condition.Date.createdBefore(getDateZero()).asString().startsWith("[mgnl:created] < cast('1970-01-01T01:00:00.000"));
+        assertTrue(Sql2.Condition.Date.createdBefore(getDateZero()).asString().startsWith("[mgnl:created] < cast('1970-01-01T01:00:00"));
         assertTrue(Sql2.Condition.Date.createdBefore(getDateZero()).asString().endsWith("' as date)"));
     }
 
     @Test
     public void dateCreatedAfter() {
         assertThat(Sql2.Condition.Date.createdAfter(null).asString(), is(""));
-        assertTrue(Sql2.Condition.Date.createdAfter(getDateZero()).asString().startsWith("[mgnl:created] > cast('1970-01-01T01:00:00.000"));
+        assertTrue(Sql2.Condition.Date.createdAfter(getDateZero()).asString().startsWith("[mgnl:created] > cast('1970-01-01T01:00:00"));
         assertTrue(Sql2.Condition.Date.createdAfter(getDateZero()).asString().endsWith("' as date)"));
     }
 
